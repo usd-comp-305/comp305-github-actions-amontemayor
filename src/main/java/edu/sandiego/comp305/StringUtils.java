e43bc5c53df4ea6c65d90674cc2c2531bad8e932
Alex Montemayor <amontemayor@sandiego.edu>
2026-04-26 21:59:44 -0700

    feat: first test passed

--- src/main/java/edu/sandiego/comp305/StringUtils.java
+++ src/main/java/edu/sandiego/comp305/StringUtils.java
@@ -1,9 +1,22 @@
 package edu.sandiego.comp305;
 
+import java.util.ArrayList;
+
 public class StringUtils {
 
 
     public static String reverseString(String word) {
-        return "";
+
+        StringBuilder letterBuffer = new StringBuilder();
+        char[] wordCharacters = word.toCharArray();
+
+        int wordSize = word.length();
+
+
+        for(int i = wordSize-1; i >= 0; i--) {
+            letterBuffer.append(wordCharacters[i]);
+        }
+
+        return letterBuffer.toString();
     }
 }
