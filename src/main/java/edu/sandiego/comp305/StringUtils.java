53368fe249e10bec0f1af72210c5f5917dc2c625
Alex Montemayor <amontemayor@sandiego.edu>
2026-04-26 21:50:04 -0700

    feat: test case 1 complete

---
+++ src/main/java/edu/sandiego/comp305/StringUtils.java
@@ -0,0 +1,9 @@
+package edu.sandiego.comp305;
+
+public class StringUtils {
+
+
+    public static String reverseString(String word) {
+        return "";
+    }
+}
---
+++ src/test/java/edu/sandiego/comp305/StringUtilsTest.java
@@ -0,0 +1,20 @@
+package edu.sandiego.comp305;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringUtilsTest {
+
+    @Test
+    void testReverseStringInvertsThreeLetterWord() {
+
+        String word = "cat";
+
+        String reversedWord = StringUtils.reverseString(word);
+
+        assertEquals("tac", reversedWord);
+
+    }
+
+}
